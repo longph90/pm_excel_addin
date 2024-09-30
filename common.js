7e991cfd091363e1dc0f7f5e8f48e3f4708076a5
LongPH90 <longph90@gmail.com>
2024-09-30 20:19:33 +0700

    Updates

--- common.js
+++ common.js
@@ -1,1 +1,2 @@
-!function(){"use strict"}();
+!function(){"use strict";Object.freeze({OPEN:"OPEN",IN_PROGRESS:"IN_PROGRESS",WAITING_REVIEW:"WAITING_REVIEW",WAITING_RELEASE:"WAITING_RELEASE",RELEASED:"RELEASED"})}();
+//# sourceMappingURL=common.js.map
